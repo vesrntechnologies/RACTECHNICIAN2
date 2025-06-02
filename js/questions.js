5e3fdabfd04d2818d6d75b58996dedb40f81d6e0
vesrntechnologies <vennjoy@gmail.com>
2025-06-02 15:39:29 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,618 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which extinguishes the fire of flammable liquids?",
+  //  immg:"img1",
+    answer: "CO2",
+    options: [
+      "Water",
+      "Air",
+      "CO2",
+      "Oil"
+    ]
+  },
+      {
+    numb: 2,
+    question: "What is the type of chisel?",
+    immg:"img2",
+    answer: "Diamond Point",
+    options: [
+      "Flat",
+      "Cross Cut",
+      "Half round",
+      "Diamond Point"
+    ]
+  },
+     {
+    numb: 3,
+    question: "Which materials are used to make mallets?",
+   
+    answer: "Wood, rubber",
+    options: [
+      "Wood, cast iron",
+      "Wood, rubber",
+      "Rubber, brass",
+      "Wood, brass"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is tje angle marked as X in sheet metal V notch?",
+    immg:"img4",
+    answer: "90o",
+    options: [
+      "90o",
+      "60o",
+      "45o",
+      "30o"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which tool is used for bending, seaming and forming of sheet metal?",
+    //immg:"img2",
+    answer: "Stakes",
+    options: [
+      "Bench vice",
+      "Hand Vice",
+      "Stakes",
+      "Shear"
+    ]
+  },
+      {
+    numb: 6,
+    question: "What is the name of the tool used for bending sheet metal?",
+    immg:"img6",
+    answer: "Blow horn stake",
+    options: [
+      "Bevel edged square stake",
+      "Blow horn stake",
+      "Hatchet stake",
+      "Square stake"
+    ]
+  },
+      {
+    numb: 7,
+    question: "What is the name of part marked as X in snips?",
+    immg:"img7",
+    answer: "Stops",
+    options: [
+      "Handle",
+      "Black",
+      "Stops",
+      "Bolt"
+    ]
+  },
+      {
+    numb: 8,
+    question: "What is the name of the soldering tool?",
+    immg:"img8",
+    answer: "Soldering gun",
+    options: [
+      "Blow lamp",
+      "Pot and ladle",
+      "Soldering gun",
+      "Soldering iron"
+    ]
+  },
+      {
+    numb: 9,
+    question: "Which tool is ued for bending long sheet metal?",
+   // immg:"img8",
+    answer: "Folding bar",
+    options: [
+      "Folding bar",
+      "Bench Vice",
+      "Hand Vice",
+      "Shear"
+    ]
+  },
+      {
+    numb: 10,
+    question: "Which tool is used for tightening the odd size bolts and nuts?",
+   // immg:"img8",
+    answer: "Adjustable spanner",
+    options: [
+      "Socket set",
+      "Nose pliers",
+      "Ring spanner",
+      "Adjustable spanner"
+    ]
+  },
+      {
+    numb: 11,
+    question: "Which tool is uded for trimming the edges of a sheet metal cylinder?",
+   // immg:"img8",
+    answer: "Bent snips",
+    options: [
+      "Straight snips",
+      "Bent snips",
+      "Tube cutter",
+      "Scissors"
+    ]
+  },
+     {
+    numb: 12,
+    question: "What is smoothering in fire extingusishing?",
+   // immg:"img8",
+    answer: "Cutting off oxygen supply to fire",
+    options: [
+      "Pouring of CTC",
+      "Flashing with dry powder",
+      "Use water to lower temperature",
+      "Cutting off oxygen supply to fire"
+    ]
+  },
+    {
+    numb: 13,
+    question: "What is starving in extingushing the fire?",
+   // immg:"img8",
+    answer: "Removal of fuel",
+    options: [
+      "Adding fuel",
+      "Removal of fuel",
+      "Clearing the hazards",
+      "Stop the supply of oxygen"
+    ]
+  },
+     {
+    numb: 14,
+    question: "What is the material composition of a Tin mans solder?",
+   // immg:"img8",
+    answer: "50% Tin 50% Lead",
+    options: [
+      "50% Tin 50% Zinc",
+      "50% Tin 50% Lead",
+      "50% Zinc 50% lead",
+      "50% Tin 50% Bronze"
+    ]
+  },
+      {
+    numb: 15,
+    question: "What is the name of part marked as X in outside micrometer?",
+    immg:"img15",
+    answer: "Barrel",
+    options: [
+      "Barrel",
+      "Spindle",
+      "Lock nut",
+      "Steel frame"
+    ]
+  },
+       {
+    numb: 16,
+    question: "Which is the point angle of a twist drill used for zinc alloy?",
+   // immg:"img15",
+    answer: "118o",
+    options: [
+      "90o",
+      "100o",
+      "118o",
+      "125o"
+    ]
+  },
+       {
+    numb: 17,
+    question: "What is the angle of the itp of centre punch?",
+   // immg:"img15",
+    answer: "90o",
+    options: [
+      "30o",
+      "60o",
+      "90o",
+      "125o"
+    ]
+  },
+      {
+    numb: 18,
+    question: "Which file is used for filing wood?",
+   // immg:"img15",
+    answer: "Smooth file",
+    options: [
+      "Double cut file",
+      "Single cut file",
+      "Smooth file",
+      "Rasp cut file"
+    ]
+  },
+
+     {
+    numb: 19,
+    question: "Which operation is in progress?",
+    immg:"img19",
+    answer: "Tapping",
+    options: [
+      "Reaming",
+      "Marking",
+      "Tapping",
+      "Drilling"
+    ]
+  },
+      {
+    numb: 20,
+    question: "Which medium isused to extiguish Class-A fire?",
+    //immg:"img19",
+    answer: "Water",
+    options: [
+      "CO2",
+      "Foam",
+      "Water",
+      "Dry powder"
+    ]
+  },
+     {
+    numb: 21,
+    question: "What is the use of the tool in bevelling a drilled hole on job?",
+    immg:"img21",
+    answer: "Counter Sinking",
+    options: [
+      "Drilling",
+      "Reaming",
+      "Counter Sinking",
+      "Counter boring"
+    ]
+  },
+    {
+    numb: 22,
+    question: "Which material is used to clean oil and grease on the floor?",
+   // immg:"img21",
+    answer: "Saw Dust",
+    options: [
+      "Cotton Waste",
+      "Saw Dust",
+      "Water",
+      "Air"
+    ]
+  },
+    {
+    numb: 23,
+    question: "What is the least count of vernier calipers?",
+    immg:"img23",
+    answer: "0.02",
+    options: [
+      "0.01",
+      "0.02",
+      "0.10",
+      "0.20"
+    ]
+  },
+      {
+    numb: 24,
+    question: "Which operation is progressed on the job?",
+    immg:"img24",
+    answer: "Reaming",
+    options: [
+      "Drilling",
+      "Marking",
+      "Tapping",
+      "Reaming"
+    ]
+  },
+     {
+    numb: 25,
+    question: "Which operation is progress by snips?",
+    immg:"img25",
+    answer: "Trimming cylinder",
+    options: [
+      "Straight cutting",
+      "Steel tube cutting",
+      "Trimming cylinder",
+      "Copper tube cutting"
+    ]
+  },
+    {
+    numb: 26,
+    question: "Which operation is in progress?",
+    immg:"img26",
+    answer: "Punching",
+    options: [
+      "Cutting",
+      "Drilling",
+      "Marking",
+      "Punching"
+    ]
+  },
+    {
+    numb: 27,
+    question: "What is the name of part marked as X in surface gauge?",
+    immg:"img27",
+    answer: "Snug",
+    options: [
+      "Nut",
+      "Snug",
+      "Base",
+      "Spindle"
+    ]
+  },
+    {
+    numb: 28,
+    question: "What is the name of the caliper having one leg with an adjustable divider point and other leg is bent?",
+    //immg:"img27",
+    answer: "Jenny Caliper",
+    options: [
+      "Jenny Caliper",
+      "Inside Caliper",
+      "Outside Caliper",
+      "Spring Joint Caliper"
+    ]
+  },
+      {
+    numb: 29,
+    question: "What is the least count of a metric micrometer?",
+    //immg:"img27",
+    answer: "0.01mm",
+    options: [
+      "0.02mm",
+      "0.01mm",
+      "0.001mm",
+      "000.1mm"
+    ]
+  },
+     {
+    numb: 30,
+    question: "What is the type of fire class?",
+    immg:"img30",
+    answer: "Class B fire",
+    options: [
+      "Class A fire",
+      "Class B fire",
+      "Class C fire",
+      "Class D fire"
+    ]
+  },
+     {
+    numb: 31,
+    question: "What is the ideal temperature for bacteria to grow slowly?",
+   // immg:"img30",
+    answer: "4o C",
+    options: [
+      "2o C",
+      "3o C",
+      "4o C",
+      "5o C"
+    ]
+  },
+      {
+    numb: 32,
+    question: "What is the method for preserving food for extended period?",
+   // immg:"img30",
+    answer: "Fast Freezing",
+    options: [
+      "Air Conditioning",
+      "Heating",
+      "Fast Freezing",
+      "Ventilation"
+    ]
+  },
+     {
+    numb: 33,
+    question: "What is the angle of teeth for a single cut file?",
+   // immg:"img30",
+    answer: "60o",
+    options: [
+      "45o",
+      "60o",
+      "70o",
+      "90o"
+    ]
+  },
+     {
+    numb: 34,
+    question: "Which tool is used for holding work pieces?",
+   // immg:"img30",
+    answer: "Bench Vice",
+    options: [
+      "Stakes",
+      "Shear",
+      "Bench Vice",
+      "Folding bar"
+    ]
+  },
+      {
+    numb: 35,
+    question: "What is the angle of blade of try square?",
+   // immg:"img30",
+    answer: "90o",
+    options: [
+      "45o",
+      "60o",
+      "70o",
+      "90o"
+    ]
+  },
+       {
+    numb: 36,
+    question: "Which hand tool is used for checking vertical alignment of wall?",
+   // immg:"img30",
+    answer: "plum bob",
+    options: [
+      "Try square",
+      "Steel rule",
+      "Measuring tape",
+      "plumb bob"
+    ]
+  },
+     {
+    numb: 37,
+    question: "What is the instrument is used for measuring the thickness of sheet metal?",
+   // immg:"img30",
+    answer: "standard wire gauge",
+    options: [
+      "steel rule",
+      "try square",
+      "standard wire gauge",
+      "measuring tape"
+    ]
+  },
+     {
+    numb: 38,
+    question: "How many types of rivets are there?",
+   // immg:"img30",
+    answer: "4",
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+       {
+    numb: 39,
+    question: "What are the two classifications of system of units?",
+   // immg:"img30",
+    answer: "Fundamental and derived",
+    options: [
+      "British and metric",
+      "Gravitational and non gravitational",
+      "Fundamental and derived",
+      "metric and international"
+    ]
+  },
+     {
+    numb: 40,
+    question: "What are fundamental units?",
+   // immg:"img30",
+    answer: "Length, Mass, Time",
+    options: [
+      "Length, Mass, Volume",
+      "Length, Mass, Time",
+      "Length, Mass, Area",
+      "Length, Pressure, Volume"
+    ]
+  },
+    {
+    numb: 41,
+    question: "What denotes letter M in MKS System?",
+   // immg:"img30",
+    answer: "Meter",
+    options: [
+      "Mile",
+      "Meter",
+      "Millimeter",
+      "Micron"
+    ]
+  },
+     {
+    numb: 42,
+    question: "How many millimeters are there in 1 inch?",
+   // immg:"img30",
+    answer: "25.4mm",
+    options: [
+      "2.54mm",
+      "25.4mm",
+      "24.5mm",
+      "2.45mm"
+    ]
+  },
+     {
+    numb: 43,
+    question: "What is the squre root of 529?",
+   // immg:"img30",
+    answer: "23",
+    options: [
+      "25",
+      "23",
+      "24",
+      "33"
+    ]
+  },
+    {
+    numb: 44,
+    question: "Convert 0.456 decimal fraction into percentage?",
+   // immg:"img30",
+    answer: "45.6%",
+    options: [
+      "45.6%",
+      "4.56%",
+      "0.456%",
+      "0.0456"
+    ]
+  },
+      {
+    numb: 45,
+    question: "Convert 0.456 decimal fraction into percentage?",
+    immg:"img45",
+    answer: "water",
+    options: [
+      "water",
+      "concrete",
+      "asbestos",
+      "glass"
+    ]
+  },
+     {
+    numb: 46,
+    question: "Identify the conventional symbol of material?",
+    immg:"img46",
+    answer: "wood",
+    options: [
+      "lead",
+      "glass",
+      "wood",
+      "paper"
+    ]
+  },
+    {
+    numb: 47,
+    question: "Identify the name of instrument?",
+    immg:"img47",
+    answer: "protractor",
+    options: [
+      "set square",
+      "protractor",
+      "french curve",
+      "T square"
+    ]
+  },
+     {
+    numb: 48,
+    question: "Identify the name of set square",
+    immg:"img48",
+    answer: "45o set square",
+    options: [
+      "15o set square",
+      "65o set square",
+      "30o set square",
+      "45o set square"
+    ]
+  },
+    {
+    numb: 49,
+    question: "Compass used to draw for____",
+   // immg:"img48",
+    answer: "circle",
+    options: [
+      "rectangle",
+      "suare",
+      "circle",
+      "triangle"
+    ]
+  },
+       {
+    numb: 50,
+    question: "a=5, b=10 then a+b=?",
+   // immg:"img48",
+    answer: "15",
+    options: [
+      "15",
+      "-5",
+      "5",
+      "-15"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
